0fefba6ad818ebd402523f09dade4f202537e4a0
bungyschaf <rene.gruenbauer@web.de>
2022-10-26 23:57:23 +0200

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -9,14 +9,16 @@ sampler = DWaveSampler(
 h = {
     (0,0): -1,  # Koppler auf Qubit 0
     (1,1):  1,  # Koppler auf Qubit 1
-    (2,2):  1,  # Koppler auf Qubit 2
+    (2,2):  0,  # Koppler auf Qubit 2
     (1,5):  1,  # Koppler zwischen Qubits 1 und 5
     (1,6): -1,  # Koppler zwischen Qubits 1 und 6
 }
 
+# Anneal starten
 response = sampler.sample_qubo(
     h, 
     num_reads=100,
 )
 
+# Loesungen ansehen
 dwave.inspector.show(response)
