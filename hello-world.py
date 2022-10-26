31321ecd8e84bc87c4def360317e6757a31e7a07
bungyschaf <rene.gruenbauer@web.de>
2022-10-27 00:08:41 +0200

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -7,8 +7,8 @@ sampler = DWaveSampler(
 
 # Setzen der Koppler h_ij
 h = {
-    (0,0): -1,  # Koppler auf Qubit 0
-    (1,1):  1,  # Koppler auf Qubit 1
+    (0,0):  1,  # Koppler auf Qubit 0
+    (1,1): -1,  # Koppler auf Qubit 1
     (2,2):  0,  # Koppler auf Qubit 2
     (1,5):  1,  # Koppler zwischen Qubits 1 und 5
     (1,6): -1,  # Koppler zwischen Qubits 1 und 6
